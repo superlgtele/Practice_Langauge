cfd9489d9cbde78180f44c08b30e02871ac9cea4
superlgtele <superlgtele@naver.com>
2021-12-07 15:41:48 +0900

    Struct
    2021/12/07

---
+++ struct.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+struct Point
+{
+    double x;
+    double y;
+};
+
+double GetDistance(struct Point p1, struct Point p2)
+{
+    double dist;
+    dist = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    return dist;
+}
+
+int main()
+{
+    double Distance;
+    struct Point p1 = {430, 170};
+    struct Point p2 = {120, 80};
+    Distance = GetDistance(p1, p2);
+    printf("distance: %lf", Distance);
+}
