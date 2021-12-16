4e0a76c7186268547e43b506aec2e80e643108ab
superlgtele <superlgtele@naver.com>
2021-12-16 14:02:20 +0900

    Install_Django
    2021/12/16

--- firstPj/firstPj/settings.py
+++ firstPj/firstPj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-re2%3imb2^)1(4^*@xfv9rtu@cj71w_-*e7%_jgtd_f=c%=c%c'
+SECRET_KEY = 'save my memofile'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
