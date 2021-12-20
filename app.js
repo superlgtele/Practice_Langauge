0906098ec3ca901c02e13df4de277d7ace85e0c4
superlgtele <superlgtele@naver.com>
2021-12-20 12:56:27 +0900

    hidden
    2021/12/20

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const loginInput = document.querySelector("#login-form input");
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add("hidden");
+    console.log(username);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
