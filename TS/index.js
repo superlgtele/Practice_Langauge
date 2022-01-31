8bc119e386fc58a11cd457a176b2b3dcf594d7f9
superlgtele <superlgtele@naver.com>
2022-01-31 14:46:46 +0900

    Narrowing
    2022/01/31

--- TS/index.js
+++ TS/index.js
@@ -9,3 +9,21 @@ function narrowing(x) {
 narrowing(123);
 narrowing("성공!");
 var 동물 = { name: "monkey", age: 5 };
+var 내이름 = {
+    name: "Honggi",
+};
+//  as const == 타입을 ""(string)이 아닌 입력한 그대로(Honggi Type) 바꾸자
+//  readonly 기능추가 -> 오브젝트 자료 재할당 불가(const 추가기능)
+function 내이름함수(a) { }
+내이름함수(내이름.name);
+var typealias = function (a) {
+    return 10;
+};
+var 저는변경 = document.querySelector("#title");
+var 버튼기능 = document.querySelector("#button");
+// ?문법 -> narrowing -> #button 있으면 함수실행, 없으면 undefined return
+버튼기능 === null || 버튼기능 === void 0 ? void 0 : 버튼기능.addEventListener("click", function () {
+    if (저는변경 != null) {
+        저는변경.innerHTML = "김홍기입니다";
+    }
+});
--- TS/index.ts
+++ TS/index.ts
@@ -5,9 +5,30 @@ function narrowing(x: number | string) {
     console.log(x + " 야호");
   }
 }
-
 narrowing(123);
 narrowing("성공!");
 
 type Animal = { name: String; age: number };
 const 동물: Animal = { name: "monkey", age: 5 };
+
+const 내이름 = {
+  name: "Honggi",
+} as const;
+//  as const == 타입을 ""(string)이 아닌 입력한 그대로(Honggi Type) 바꾸자
+//  readonly 기능추가 -> 오브젝트 자료 재할당 불가(const 추가기능)
+function 내이름함수(a: "Honggi") {}
+내이름함수(내이름.name);
+
+type 함수타입 = (a: string) => number;
+const typealias: 함수타입 = function (a) {
+  return 10;
+};
+
+let 저는변경 = document.querySelector("#title");
+let 버튼기능 = document.querySelector("#button");
+// ?문법 -> narrowing -> #button 있으면 함수실행, 없으면 undefined return
+버튼기능?.addEventListener("click", function () {
+  if (저는변경 != null) {
+    저는변경.innerHTML = "김홍기입니다";
+  }
+});
